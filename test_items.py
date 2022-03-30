bdc9a194fe230b04925114d3a5efbb574d262e03
lambotik <lambotik1988@gmail.com>
2022-03-31 00:57:15 +0300

    Закончил раздел 3.6

---
+++ test_items.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.chrome.options import Options
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+
+
+def test_basket(browser):
+    browser.get(link)
+    basket=browser.find_element_by_css_selector(".btn.btn-add-to-basket")
+    assert basket,"basket not found"
+
