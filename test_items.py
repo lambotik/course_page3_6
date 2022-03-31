37b0f2011cb5efa72aacaba2a4ead3eab9d488c0
lambotik <lambotik1988@gmail.com>
2022-03-31 17:10:39 +0300

    Добавил implicity_wait

--- test_items.py
+++ test_items.py
@@ -1,10 +1,7 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-
-
 def test_basket(browser):
     browser.get(link)
     basket=browser.find_element_by_css_selector(".btn.btn-add-to-basket")
     assert basket,"basket not found"
-
